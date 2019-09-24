bca54f8c4122cab5ba2d53bbff8d725ec2aa18aa
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2019-09-24 10:22:28 +0100

    Create process1.sh

---
+++ process1.sh
@@ -0,0 +1,22 @@
+import os
+os.system('./terminal1.bash')
+
+
+
+
+
+
+
+
+
+
+'''while True:
+    try:
+        os.system('gnome-terminal ; sudo screen /dev/yi-serial-b 115200')
+    except ValueError:
+        os.system('sudo rmmod ftdi_sio')
+        os.system('bash pause')
+        os.system('sudo screen /dev/yi-serial-b 115200')
+        ## it will hags if serial cable is not fund
+        
+###explore the ideaof using a bach file'''
